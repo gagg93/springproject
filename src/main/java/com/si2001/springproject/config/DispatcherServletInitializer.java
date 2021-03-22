07d5b83f836adccdcee6719d896845701f172481
Si2001lorenzo.gaggero <gaggero.lorenzo@gmail.com>
2021-03-22 15:42:34 +0100

    Initial commit

---
+++ src/main/java/com/si2001/springproject/config/DispatcherServletInitializer.java
@@ -0,0 +1,30 @@
+package com.si2001.springproject.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class[]
+                {
+                        WebApplicationContextConfig.class
+                };
+    }
+
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[] { "/" };
+    }
+
+}
